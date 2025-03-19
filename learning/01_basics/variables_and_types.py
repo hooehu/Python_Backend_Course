4623ff29aeb6b4867eaa4ab7c6a0306d6412137f
hooehu <imalyshev03@gmail.com>
2025-03-19 18:42:57 +0300

    3

--- learning/01_basics/variables_and_types.py
+++ learning/01_basics/variables_and_types.py
@@ -12,11 +12,11 @@ name = "Ivan"
 # bool - логическое значение
 is_active = True
 
-# list - списки (массивы)
-numbers = [1, 2, 3]
+# list - списки (массивы).
+numbers = [1, 2, 'hello']
 
 # tuple - кортежи (неизменяемые списки)
-coordinates = (10, 20)
+t_tuple = (10, 20, 'hello')
 
 # dict - словари (ключ-значение)
 person = {
@@ -24,4 +24,5 @@ person = {
     age: "21",
 }
 
-#
+# set - множество (запрещены дубликаты)
+s_set = {"a", 3}
